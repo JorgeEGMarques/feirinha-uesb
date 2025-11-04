0be22ed1051f8362ce0f37c9e9e86c9b1538d702
CarlosHenriqueSL <luiscarloslopes597@gmail.com>
2025-11-04 14:03:13 -0300

    Enviando estado inicial do back-end para o repositório.

---
+++ src/main/java/model/entities/Reservation.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Reservation {
+    private long code;
+    private String holderCpf;
+    private LocalDate reservationDate;
+
+    public Reservation(){}
+    public Reservation(long code, String holderCpf, LocalDate reservationDate){
+
+    }
+}
